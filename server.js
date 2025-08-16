6c217879dbd3c97567a36ae1a510bb3e156719d2
suraj <sharadadde@gmail.com>
2025-08-16 16:23:29 +0530

    Connected the mongodbatlas

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log("DB Error:", err));
+
+app.get("/", (req, res) => {
+    res.send("File Sharing App Backend is running");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
